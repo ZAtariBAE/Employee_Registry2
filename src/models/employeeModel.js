79415f7585d929524c9d40ad5f88233a4ee9cf96
ZAtariBAE <z.attari@bankaletihad.com>
2026-04-20 17:46:09 +0300

    data model init

--- src/models/employeeModel.js
+++ src/models/employeeModel.js
@@ -1,2 +1,19 @@
 // SQL Controller
 // Executes SQL Queries
+
+const {DataTypes} = require('sequelize');
+
+// https://blog.postman.com/how-to-create-a-rest-api-with-node-js-and-express/
+
+const UserModel = {
+    id: {type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true},
+    name: { type: DataTypes.STRING, allowNull: false },
+    department : { type: DataTypes.STRING, allowNull: false },
+    salary: { type: DataTypes.FLOAT, allowNull: false },
+    email: { type: DataTypes.STRING, allowNull: true },
+    created_at: { type: DataTypes.TIME, allowNull: false },
+    updated_at: { type: DataTypes.TIME, allowNull: false },
+    status: { type: DataTypes.STRING , allowNull: false },
+}
+
+module.exports = (sequelize) => sequelize.define('user', UserModel);
